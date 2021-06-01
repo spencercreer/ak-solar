d4449f432ac321c34739d5e086695972b80ebec7
spencercreer <spencercreer@gmail.com>
2021-06-01 00:04:40 -0600

    styling

---
+++ eight-k-solar/src/App.js
@@ -0,0 +1,18 @@
+import EksNavbar from './components/EksNavbar/EksNavbar';
+import EksTitle from './components/EksTitle/EksTitle';
+// import EksForm from './components/EksForm/EksForm';
+import EksFooter from './components/EksFooter/EksFooter';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <EksNavbar />
+    <EksTitle />
+    {/* <EksForm /> */}
+    <EksFooter />
+    </>
+  );
+}
+
+export default App;
